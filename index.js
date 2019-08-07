9859a367443e3b02635f23111d93f3b416e4e409
Marcus Kwan <kwan.marcus@gmail.com>
2019-08-08 06:54:28 +1000

    added get

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+
+const server = require("./server.js");
+
+const port = 8000;
+const greeting = "API IS RUNNING AHHHHH!!!!";
+server.listen(port, () => {
+    console.log(
+        `\n*** ${greeting} Server Running on http://localhost:${port} ***\n`
+    );
+});
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = 'express';
+const express = require('express')
 
 const server = express();
+
+const userRoutes = require('./users/userRouter')
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
