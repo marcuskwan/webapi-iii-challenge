ef818f5972908e7bc888fba08a4589f98c239a3f
Marcus Kwan <kwan.marcus@gmail.com>
2019-08-09 06:03:59 +1000

    added env files and updated json for deployment to heroku

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // code away!
 
+// use environment variables
+require('dotenv').config()
+
 const server = require("./api/server");
 
-const port = 8000;
+// read port from server environment if it's there... or PORT 8000 .. Heroku will have the PORT environment variable set
+const port = process.env.PORT || 8000;
+
 const greeting = "API IS RUNNING AHHHHH!!!!";
 server.listen(port, () => {
     console.log(
